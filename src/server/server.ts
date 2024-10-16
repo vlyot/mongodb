85d2c5ef1d97d17acc65894b787701195dab766a
vlyot <ngkaichong10@gmail.com>
2024-10-16 18:32:38 +0800

    l

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,2 +1,16 @@
 import express from "express";
-console.log(express);
+
+const server = express();
+
+server.use(express.static("dist"));
+server.use("/", (req,res) =>{
+    res.render("index",{
+        content: "EJS is real"
+    });
+});
+server.set("view engine", "ejs")
+
+
+server.listen("3000","localhost",()=>{
+    console.info("Express server is listening at http://localhost:3000");
+});
